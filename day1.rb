641598a7fb5ed018e2fbd3dbb60f21fbeb22fe2a
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-08 18:03:33 +1100

    day 1 complete

--- day1.rb
+++ day1.rb
@@ -1,36 +1,37 @@
-input = 'day1-input.txt'
+input_data = 'day1-input.txt'
 
 input_arr = []
-File.foreach(input) { |line| input_arr << line.chomp}
+File.foreach(input_data) { |line| input_arr << line.chomp}
 
 example = ['L68', 'L30', 'R48', 'L5', 'R60', 'L55', 'L1', 'L99', 'R14', 'L82']
-zeros = 0
-value = 50
 
-example.each do |input|
-  p input
-  if input.chr == 'R'
-    num = input.delete('R')
-    i = num.to_i
-    value += i
-    while value > 99
-      zeros += 1
-      value -= 100
-    end
-    zeros += 1 if value == 0
+zeros = 0
+start = 50
+
+def zero_hits_right(start, steps)
+  first = (100 - start) % 100
+  first = 100 if first == 0
+
+  return 0 if steps < first
+  1 + (steps - first) / 100
+end
+
+def zero_hits_left(start, steps)
+  zero_hits_right((100 - start) % 100, steps)
+end
+
+input_arr.each do |input|
+  direction = input[0]
+  steps = input[1..].to_i
+
+  if direction == 'R'
+    zeros += zero_hits_right(start, steps)
+    start = (start + steps) % 100
   else
-    num = input.delete('L')
-    i = num.to_i
-    value -= i
-    while value < 0
-      zeros += 1
-      value += 100
-    end
-    zeros += 1 if value == 0
+    zeros += zero_hits_left(start, steps)
+    start = (start - steps) % 100
+    start += 100 if start < 0
   end
-  
-  p value
-  p "zeros: #{zeros}"
 end
 
 p zeros
