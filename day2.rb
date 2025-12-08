8dd7d7ee6d81572cfb4e9339e7ae92e35da359e7
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-09 07:37:41 +1100

    bugs to sort out

--- day2.rb
+++ day2.rb
@@ -16,8 +16,22 @@ end
 
 total = 0
 
-def check_num(num, mid, last) 
-  true if num[0...mid] == num[mid..last]
+def check_num(dig) 
+  # true if num[0...mid] == num[mid..last] 
+  n = dig.length
+  num = dig.join
+  half = n/2
+  range = (1..half)
+
+  range.each do |size|
+    next unless n % size == 0
+
+    pattern = num[0, size]
+    return true if pattern * (n/size) == num
+  end
+  # next, to check all possible combinations of duplicates (so the same number repeated, same set of numbers repeated.)
+  # Now, an ID is invalid if it is made only of some sequence of digits repeated at least twice. 
+  # So, 12341234 (1234 two times), 123123123 (123 three times), 1212121212 (12 five times), and 1111111 (1 seven times) are all invalid IDs.
 end
 
 until final_input.empty?
@@ -29,7 +43,7 @@ until final_input.empty?
     num_arr = num_to_array(current_num)
     last = num_arr.length
     mid = last / 2
-    total += current_num if check_num(num_arr, mid, last)
+    total += current_num if check_num(num_arr)
     current_num += 1
   end
 
