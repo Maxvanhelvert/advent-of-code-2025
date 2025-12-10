2a324f91c87de62605b00f8a0e280f9074d7a779
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-10 11:14:47 +1100

    add initial solution for part 2

--- day3-2.rb
+++ day3-2.rb
@@ -13,42 +13,30 @@ def start
     batteries = input.split('')
     
     length = batteries.length
-    first_num_index = 0
+	number_of_digits = 12
+	current_digit = 0
+    num_index = 0
     start = 0
-    first_num = '0'
-    sec_num = '0'
+    num = '0'
+    total_num = '0'
 
-    while start < length - 1
-      if batteries[start] > first_num
-        first_num = batteries[start] 
-        first_num_index = start
-        sec_num = batteries[start+1]
-      end
-      start += 1
-    end
-    second = first_num_index + 1
+	while current_digit < number_of_digits 
+		while start < length - (number_of_digits - current_digit)
+		  if batteries[start] > num
+			num = batteries[start] 
+			num_index = start
+		  end
+		  start += 1
+		end
+		total_num = total_num + num
+		current_digit += 1
+		num = '0'
+	end
 
-    while second < length
-      if batteries[second] > sec_num
-        sec_num = batteries[second]
-      end
-      second += 1
-    end
-    final_num = (first_num + sec_num).to_i
+    final_num = (total_num).to_i
     total += final_num
   end
-  # get first value of input array, 
-  # change str to array of integers
-  # assign first value of array to ver first_num, assign its index to first_num_index
-  # look through array from 0 to length -2 (leave off last index), assign value to first_num if current > first_num
-  # when finished, look through array for sec_num in range (first_num_index + 1...length)
-  # assign value to sec_num if value > sec_num
-  # 
-  # num = first_num.append(sec_num)
-  # total += num
-  # 
-  # loop for each value in input
   puts total
 end
 
-start
+start #current answer too low ... ?
