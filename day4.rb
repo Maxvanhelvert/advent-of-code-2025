723545b8a92ac013101de8437c7c70fd193c78b6
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-11 10:53:07 +1100

    add start func w/ first and last row

--- day4.rb
+++ day4.rb
@@ -2,8 +2,15 @@ def get_input
   input_data = 'day4-input.txt'
 
   input_arr = []
-  File.foreach(input_data) { |line| input_arr << line.chomp}
+  File.foreach(input_data) { |line| input_arr << line.chomp.split('')}
   input_arr
 end
 
-puts get_input
+def start
+	storage_floor = get_input
+	first_row = storage_floor[1]
+	last_row = storage_floor[storage_floor.length - 1]
+	p last_row
+end
+
+start
