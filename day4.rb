84b39365b37873b6b52c287b38d41fe5ac10726f
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-11 21:43:30 +1100

    initial version for day 4 giving incorrect answer

--- day4.rb
+++ day4.rb
@@ -8,9 +8,27 @@ end
 
 def start
 	storage_floor = get_input
-	first_row = storage_floor[1]
-	last_row = storage_floor[storage_floor.length - 1]
-	p last_row
+  total_rolls = 0
+
+	storage_floor.each_with_index do |row, r|
+    row.each_with_index do |cell, c|
+      next unless cell == '@'
+      n = 0
+      
+      n += 1 if storage_floor[r-1][c-1]
+      n += 1 if storage_floor[r-1][c]
+      n += 1 if storage_floor[r-1][c+1]
+      n += 1 if storage_floor[r][c-1]
+      n += 1 if storage_floor[r][c+1]
+      next if r == 136
+      n += 1 if storage_floor[r+1][c-1]
+      n += 1 if storage_floor[r+1][c]
+      n += 1 if storage_floor[r+1][c+1]
+      
+      total_rolls += 1 if n < 4
+    end
+  end
+  total_rolls
 end
 
-start
+p start
