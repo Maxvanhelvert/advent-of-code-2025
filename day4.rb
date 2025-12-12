d4da9f44771e64aae244e8477ba494fc79475a78
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-12 11:28:09 +1100

    add line skip for last row to avoid == nil error

--- day4.rb
+++ day4.rb
@@ -7,24 +7,24 @@ def get_input
 end
 
 def start
-	storage_floor = get_input
+  storage_floor = get_input
   total_rolls = 0
 
-	storage_floor.each_with_index do |row, r|
-    row.each_with_index do |cell, c|
+  storage_floor.each_with_index do |row, r|
+	  row.each_with_index do |cell, c|
       next unless cell == '@'
-      n = 0
+			n = 0
       
-      n += 1 if storage_floor[r-1][c-1]
-      n += 1 if storage_floor[r-1][c]
-      n += 1 if storage_floor[r-1][c+1]
-      n += 1 if storage_floor[r][c-1]
-      n += 1 if storage_floor[r][c+1]
-      next if r == 136
-      n += 1 if storage_floor[r+1][c-1]
-      n += 1 if storage_floor[r+1][c]
-      n += 1 if storage_floor[r+1][c+1]
-      
+      n += 1 if storage_floor[r-1][c-1] == '@'
+      n += 1 if storage_floor[r-1][c] == '@'
+      n += 1 if storage_floor[r-1][c+1] == '@'
+      n += 1 if storage_floor[r][c-1] == '@'
+      n += 1 if storage_floor[r][c+1] == '@'
+			if r < 136
+				n += 1 if storage_floor[r+1][c-1] == '@'
+				n += 1 if storage_floor[r+1][c] == '@'
+				n += 1 if storage_floor[r+1][c+1] == '@'
+      end 
       total_rolls += 1 if n < 4
     end
   end
@@ -32,5 +32,6 @@ def start
 end
 
 p start
-# first answer 6528(or so) and 7829
-# second answer 1284
+# first answer 6528(or so) and 7829 too high
+# second answer 1284 too low
+# third answer 1333 too low
