af461522a9442efddf78ae0995859ac809b8a57a
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-12 21:54:16 +1100

    fist answer attempt

--- day5.rb
+++ day5.rb
@@ -15,7 +15,31 @@ def get_id
 
   id_arr = []
   File.foreach(input_data) do |line|
-    id_arr << line.chomp unless line.empty? || line.include?('-')
+    id_arr << line.chomp unless line.include?('-')
   end
+  id_arr.shift
   id_arr
 end
+
+def start
+  fresh_items = 0
+  ids = get_id
+  fresh = get_fresh
+
+  fresh.each do |fresh_id|
+    fresh_range = fresh_id.split('-')
+    first = fresh_range[0]
+    last = fresh_range[1]
+
+    ids.each do |id|
+      if id >= first && id <= last
+        ids.delete(id)
+        fresh_items += 1
+      end
+    end
+  end
+  fresh_items
+end
+
+p start
+# first answer 901 too high
