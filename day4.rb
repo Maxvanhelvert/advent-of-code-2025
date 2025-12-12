b69571b3873a4d2ef4d951074e05c74bd0f62165
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-12 16:58:07 +1100

    add day 4 part 2 functionality

--- day4.rb
+++ day4.rb
@@ -7,31 +7,45 @@ def get_input
 end
 
 def start
-  storage_floor = get_input
+	storage_floor = get_input
+  rows = storage_floor.length
+  cols = storage_floor[0].length
   total_rolls = 0
 
-  storage_floor.each_with_index do |row, r|
-	  row.each_with_index do |cell, c|
-      next unless cell == '@'
-			n = 0
-      
-      n += 1 if storage_floor[r-1][c-1] == '@'
-      n += 1 if storage_floor[r-1][c] == '@'
-      n += 1 if storage_floor[r-1][c+1] == '@'
-      n += 1 if storage_floor[r][c-1] == '@'
-      n += 1 if storage_floor[r][c+1] == '@'
-			if r < 136
-				n += 1 if storage_floor[r+1][c-1] == '@'
-				n += 1 if storage_floor[r+1][c] == '@'
-				n += 1 if storage_floor[r+1][c+1] == '@'
-      end 
-      total_rolls += 1 if n < 4
+  loop do 
+    removable_rolls = 0
+
+    storage_floor.each_with_index do |row, r|
+      row.each_with_index do |cell, c|
+        next unless cell == '@'
+        n = 0
+        
+        # check all 8 neighbours safely
+        (-1..1).each do |dr|
+          (-1..1).each do |dc|
+            next if dr == 0 && dc == 0   # skip the cell itself
+
+            nr = r + dr
+            nc = c + dc
+
+            next if nr < 0 || nr >= rows
+            next if nc < 0 || nc >= cols
+
+            n += 1 if storage_floor[nr][nc] == '@'
+          end
+        end
+        if n < 4
+          storage_floor[r][c] = 'X'
+          removable_rolls += 1
+        end
+      end
     end
+    break if removable_rolls == 0
+    total_rolls += removable_rolls
   end
   total_rolls
 end
 
 p start
-# first answer 6528(or so) and 7829 too high
-# second answer 1284 too low
-# third answer 1333 too low
+# first answer 6528(or so) and 7829
+# second answer 1284
