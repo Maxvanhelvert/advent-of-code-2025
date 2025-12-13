354be479cc40ab854e36c4e8f67b4ef42e035cdf
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-13 13:37:26 +1100

    add final answer

--- day5.rb
+++ day5.rb
@@ -7,7 +7,9 @@ def get_fresh
       fresh_arr << line.chomp
     end
   end
-  fresh_arr
+  fresh_range = []
+  fresh_arr.each {|range| fresh_range << range.split('-')}
+  fresh_range
 end
 
 def get_id
@@ -18,7 +20,9 @@ def get_id
     id_arr << line.chomp unless line.include?('-')
   end
   id_arr.shift
-  id_arr
+  final = []
+  id_arr.each {|id| final << id.to_i}
+  final
 end
 
 def start
@@ -26,20 +30,21 @@ def start
   ids = get_id
   fresh = get_fresh
 
-  fresh.each do |fresh_id|
-    fresh_range = fresh_id.split('-')
-    first = fresh_range[0]
-    last = fresh_range[1]
-    ids.each do |id|
+  fresh.each do |fresh_range|
+    first = fresh_range[0].to_i
+    last = fresh_range[1].to_i
+
+    ids.each_with_index do |id, i|
+      next if id == 'x'
       if id >= first && id <= last
-        ids.delete(id)
+        ids[i] = 'x'
         fresh_items += 1
       end
     end
-    p ids.length
   end
   fresh_items
 end
 
 p start
 # first answer 901 too high
+# second answer 937
