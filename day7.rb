2d420dc96b2a2e4d1f41132cb3a6688af05be372
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-14 20:17:24 +1100

    add day 7 part 2

--- day7.rb
+++ day7.rb
@@ -6,8 +6,30 @@ def get_input
   input_arr
 end
 
-def start
-  tachyon = get_input
+def paths(tachyon)
+  return 1 if tachyon.is_a?(String)
+  possibilities = 0
+    tachyon.each_with_index do |line, i|
+    line.each_with_index do |pos, j|
+      if pos == 'S' && tachyon[i+1][j] != '|'
+        tachyon[i+1][j] = '|'
+      elsif pos == '^' && tachyon[i-1][j] == '|' && tachyon[i][j-1] != '|' && tachyon[i][j+1] != '|'
+        tachyon[i][j-1] = '|'
+        tachyon[i][j+1] = '|'
+        possibilities += paths(tachyon)
+      elsif pos == '.' && tachyon[i-1][j] == '|'
+        tachyon[i][j] = '|'
+      else 
+      next
+      end
+    end
+  end
+  return possibilities
 end
 
-start
+def start
+  tachyon = get_input
+  paths(tachyon)
+end
+
+p start
