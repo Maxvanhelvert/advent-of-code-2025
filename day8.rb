a60c3001075041606afcb25d491e4cf145833c48
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-15 20:58:51 +1100

    initial commit day 8

---
+++ day8.rb
@@ -0,0 +1,16 @@
+def get_input
+  input_data = 'day8-input.txt'
+
+  input_arr = []
+  File.foreach(input_data) { |line| input_arr << line.chomp.split(',')}
+  input_arr.map! do |line|
+    line.map! { |val| val.to_i}
+  end
+  input_arr
+end
+
+def start
+  boxes = get_input
+end
+
+p start
