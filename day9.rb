0300a645632eb3335211faf6e3328df0a6611538
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-19 17:12:25 +1100

    final answer day 9 part 1

--- day9.rb
+++ day9.rb
@@ -9,4 +9,29 @@ def get_input
   input_arr
 end
 
-p get_input
+def get_area(first, second)
+  dx = (second[0] - first[0]).abs + 1
+  dy = (second[1] - first[1]).abs + 1
+
+  dx * dy
+end
+
+def start
+  tiles = get_input
+
+  largest_area = 0
+
+  tiles.each do |corner_one|
+    tiles.each do |corner_two|
+      area = get_area(corner_one, corner_two)
+
+      largest_area = area if area > largest_area
+    end
+  end
+
+  largest_area
+end
+
+p start
+
+# first answer 4781377701
