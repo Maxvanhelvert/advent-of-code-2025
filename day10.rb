e36b3ee42ec628d7bed03b6c1e1d0c06af81ed84
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-12-21 15:40:34 +1100

    initial commit day 10

---
+++ day10.rb
@@ -0,0 +1,46 @@
+def get_input(request)
+  input_data = 'day10-input.txt'
+
+  brackets = ['[', ']', '(', ')', ',', ' ']
+
+  lights = []
+  buttons = []
+
+  File.foreach(input_data) do |line|
+    temp_buttons = []
+    temp = []
+
+    line.each_char do |char|
+
+      temp << char unless brackets.include?(char)
+
+      if char == ']' 
+        lights << temp
+        temp = []
+      end
+
+      if char == ')' 
+        temp.map! { |char| char.to_i}
+        temp_buttons << temp
+        temp = []
+      end
+
+      break if char == '{'
+    end
+
+    buttons << temp_buttons
+  end
+
+  return lights if request == 'lights'
+  return buttons if request == 'buttons'
+end
+
+def start
+  lights = get_input('lights')
+  buttons = get_input('buttons')
+
+  p lights
+  p buttons
+end
+
+start
